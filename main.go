3fd1c6e2e10229998c80bbd9b03c71b375643a0c
Christian Pehle <christian.pehle@gmail.com>
2017-05-27 22:55:10 +0200

    Trivial go processes

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func pixel(x int, y int, r int, g int, b int) string {
 }
 
 func pixel_client(server string, x int, y int, r int, g int, b int, stride int) {
-	conn, err := net.Dial("tcp",)
+	conn, err := net.Dial("tcp", server)
 	if err != nil {
 		fmt.Printf("pixel server %d %d not created\n", x, y)
 		return
@@ -18,7 +18,7 @@ func pixel_client(server string, x int, y int, r int, g int, b int, stride int) 
 	for {
 		for px := x; px < x+stride; px++ {
 			for py := y; py < y+stride; py++ {
-				bytes := []byte(pixel(x, y, r, g, b))
+				bytes := []byte(pixel(px, py, r, g, b))
 				conn.Write(bytes)
 			}
 		}
@@ -26,13 +26,14 @@ func pixel_client(server string, x int, y int, r int, g int, b int, stride int) 
 }
 
 func main() {
-        runtime.GOMAXPROCS(16)
-	server := "94.45.231.39:1234"
+	runtime.GOMAXPROCS(16)
+	server := "0.0.0.0:8080"
 	fmt.Printf("PX %d %d %02x%02x%02x\n", 500, 500, 10, 20, 30)
 	
-	for i := 0; i < 20; i++ {
-		for j := 0; j < 20; j++ {
-			go pixel_client(server, 500+5*i, 500+5*j, 0, 0, 0, 5)
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			stride := 20
+			go pixel_client(server, 500+stride*i, 500+stride*j, 0, 255, 0, stride)
 		}
 	}
 	for {
